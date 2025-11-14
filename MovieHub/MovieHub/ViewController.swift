339f5f7def90143b66d6c433a3a03256beffa3fb
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2025-11-14 20:18:41 +0530

    commit with initial project creation

---
+++ MovieHub/MovieHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHub
+//
+//  Created by Keerthika on 14/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieHub/MovieHubTests/MovieHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubTests.swift
+//  MovieHubTests
+//
+//  Created by Keerthika on 14/11/25.
+//
+
+import Testing
+@testable import MovieHub
+
+struct MovieHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
